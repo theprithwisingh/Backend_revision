a0b1db27f777b2a6720e5a367340550eecc61424
theprithwisingh <prithwisingh77@gmail.com>
2024-05-12 16:45:49 +0530

    path module

--- myproject/index.js
+++ myproject/index.js
@@ -30,8 +30,33 @@ let pathToFile = path.format({
 
 console.log('pathToFile >', pathToFile)
 
-console.log('IsAbsolute ->',path.isAbsolute(currentFilePath));
-console.log('IsAbsolute ->',path.isAbsolute('\index.js'));//bocz it's relative
-console.log('IsAbsolute ->',path.isAbsolute('.\index.js'));//bocz it's relative
-console.log('IsAbsolute ->',path.isAbsolute('..\index.js'));//bocz it's relative
+console.log('IsAbsolute ->',path.isAbsolute(currentFilePath));//true
+console.log('IsAbsolute ->',path.isAbsolute('\index.js'));//false->bocz it's relative
+console.log('IsAbsolute ->',path.isAbsolute('.\index.js'));//false->bocz it's relative
+console.log('IsAbsolute ->',path.isAbsolute('..\index.js'));//flase->bocz it's relative
 
+let pathToDir = path.join('\home','js','dist','index.js')
+console.log('pathToDir >', pathToDir);
+
+console.log('parse >', path.parse(currentFilePath));/*
+parse > {
+    root: 'c:\\',
+    dir: 'c:\\Users\\dl\\Desktop\\Backend\\myproject',
+    base: 'index.js',
+    ext: '.js',
+    name: 'index'
+     }
+*/  
+
+const fromPath = '/home/user/config';
+const toPath = '/home/user/js';
+console.log(path.relative(fromPath, toPath));//    ..\js
+
+console.log('resolve >', path.resolve());
+//resolve > C:\Users\dl\Desktop\Backend
+
+
+const originalPath = '/home/user/.././documents/file.txt';
+const normalizedPath = path.normalize(originalPath);
+console.log("normalizedPath >", normalizedPath);
+//normalizedPath > \home\documents\file.txt
