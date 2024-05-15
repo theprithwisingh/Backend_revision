99e0dde484fb650cedb4f7e4dc29254400c79f98
theprithwisingh <prithwisingh77@gmail.com>
2024-05-15 10:30:25 +0530

    http module

---
+++ httpModule/index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+console.log(http);
+
+const server = http.createServer((req,res)=>{
+    if(req.url=='/'){
+        res.write('<h1>helllo world</h1>')
+    }
+    else if(req.url=='/about'){
+        res.write('<h1>ABOUT page</h1>')
+    }
+    res.end();
+})
+server.listen(5003);
+console.log('the http server is running on port 5002')
