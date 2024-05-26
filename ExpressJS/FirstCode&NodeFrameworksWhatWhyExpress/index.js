e2b913e9dc7eeb227edf670e76db732cc0b8ab4a
theprithwisingh <prithwisingh77@gmail.com>
2024-05-26 23:07:35 +0530

    express

--- ExpressJS/FirstCode&NodeFrameworksWhatWhyExpress/index.js
+++ ExpressJS/FirstCode&NodeFrameworksWhatWhyExpress/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app  = express();
 
-const PORT = 3000;
+const PORT = 3020;
 const HOSTNAME = 'localhost';
 
 app.get('/', (req,res)=>{
     res.send('Hello World');
 })
+app.get('/about/newPage', (req,res)=>{
+    res.send('New Pages!');
+})
+app.get('/about', (req,res)=>{
+    res.send('About Page!');
+})
 
+app.get('/contact', (req,res)=>{
+    res.send('Contact Page!');
+})
+
+app.get('/pages', (req,res)=>{
+    res.send('their are many pages');
+})
 app.listen(PORT, ()=>{
     console.log(`Server running at ${HOSTNAME}: ${PORT}`)
 });
