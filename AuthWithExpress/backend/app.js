bdd4b789ef55ae3761428589403dc09551ceffba
theprithwisingh <prithwisingh77@gmail.com>
2024-06-06 23:45:02 +0530

    controller and app

--- AuthWithExpress/backend/app.js
+++ AuthWithExpress/backend/app.js
@@ -2,6 +2,6 @@ const express  = require('express');
 const app = express();
 
 app.use('/',(req,res)=>{
-    res.status(200).json({data:'JWTauth server updated updated'})
+    res.status(200).json({data:'JWTauth server updated'})
 });
 module.exports = app;
