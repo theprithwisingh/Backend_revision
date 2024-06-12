340ae191c63aab0ec21b3aefc5920dd74e9a1a3a
theprithwisingh <prithwisingh77@gmail.com>
2024-06-12 23:07:52 +0530

    signIn Controller

--- AuthWithExpress/backend/controller/autoController.js
+++ AuthWithExpress/backend/controller/autoController.js
@@ -4,22 +4,39 @@ const emailValidator = require("email-validator")
 const signup = async (req, res, next)=>{
     const {name,email, password,confirmPassword} = req.body;
     console.log(name,email, password,confirmPassword);
+    //Every field is required-logic
     if (!name ||!email||!password ||!confirmPassword ) {
         return res.status(400).json({
             success:false,
             message:'Every field is required'
         })
     }
+   //Please provide a valid email id -logic
     const validEmail = emailValidator.validate(email);
-    try {
-        const userInfo = userModel(req.body);
+    if (!validEmail) {
+        return res.status(400).json({
+            success:false,
+            message:'Please provide a valid email id'
+        })
+    }
+    //Password and confirm password doesn't match-logic
+    if (password!==confirmPassword) {
+        return res.status(400).json({
+            success:false,
+            message:"Password and confirm password doesn't match"
+        })
+    }
+
+
+try {
+    const userInfo = userModel(req.body);
     const result = await userInfo.save();
     return res.status(200).json({
-
     sucess:true,
     data:result
 });
     } catch (e) {
+        //Account already exists with provided email id -logic
         if (e.code===11000) {
             return res.status(400).json({
                 sucess:false,
@@ -34,6 +51,34 @@ const signup = async (req, res, next)=>{
     
 }
 
+//SIGNIN CONTROLLER
+const signin =async(req, res)=>{
+    const {email, password}  =req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success:false,
+            message:'Every field is mandatory'
+        })
+    }
+   //send req to database for valid user and passward 
+    const user = await userModel
+          .findOne({
+            email
+          })
+          .select('+password');
+
+    if (!user|| user.password!==password) {
+        return res.status(400).json({
+            success:false,
+            message:'invalid credentials'
+        })
+    }
+    
+    
+}
+
 module.exports = {
-    signup
+    signup,
+    signin
 }
