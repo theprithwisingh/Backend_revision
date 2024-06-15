67bf76e2972f6de23e73862625b5205329b8fb22
theprithwisingh <prithwisingh77@gmail.com>
2024-06-15 23:43:56 +0530

    cors

--- AuthWithExpress/backend/app.js
+++ AuthWithExpress/backend/app.js
@@ -2,10 +2,19 @@ const express  = require('express');
 const app = express();
 const authRouter = require('./router/autoRoute');
 
-const databaseconnect = require('./config/databaseConfig')
+const databaseconnect = require('./config/databaseConfig');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 databaseconnect();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:[process.env.CLIENT_URL],
+    credentials:true
+}));
+
 app.use('/api/auth',authRouter)
 app.use('/',(req,res)=>{
     res.status(200).json({data:'JWTauth server updated'})
